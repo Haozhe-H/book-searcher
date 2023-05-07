982e4ae7e018f1bd16131b84660fc4e573fd4364
Haozhe-H <haozhehuang@outlook.com>
2023-05-07 00:05:34 -0400

    update query and mutations in resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,83 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("books");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Please login first.");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError(
+          "Failed to login, please re-enter username and password."
+        );
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError(
+          "Failed to login, please re-enter username and password."
+        );
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, args) => {
+      try {
+        const user = await User.create(args);
+        const token = signToken(user);
+
+        return { token, user };
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true }
+        ).populate("books");
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("Please login first.");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("Please login first.");
+    },
+  },
+};
+
+module.exports = resolvers;
